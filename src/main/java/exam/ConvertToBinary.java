d8a16adb784ee16bc5855560846e2937b89238bf
pranjal9798 <86240983+pranjal9798@users.noreply.github.com>
2021-06-21 23:42:17 -0700

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,26 @@
-package exam;
+import java.io.*;
 
-public class ConvertToBinary {
+ class ConvertToBinary {
 
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+   static void defToBinary(int n)
+   {
+     int[] binary Num = new int [35];
+     int i = 0;
+     while (n > 0)
+     {
+       binaryNum[i] = n % 2;
+       n = n / 2;
+       i++;
+     }
+     
+     for(int j = i - 1; j >= 0; j--)
+       System.out.print(binaryNum[j]);
+   }
+     
+     
+  public static void main(String[] args) {
+    {
+      int n = 35;
+      decToBinary(n);
+    }
 }
